70ca653f4e9f087333d383f193880d39b18e6e0a
Bilski311 <dominik.bilski24@gmail.com>
2024-03-15 22:25:00 +0100

    Create storage container.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,17 @@ resource "azurerm_resource_group" "ml-auto-store-rg" {
   name     = "ml-auto-store-rg"
   location = "polandcentral"
 }
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "ml-auto-store-storage-account"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "ml-auto-store-container"
+  storage_account_name  = storage_account.name
+  container_access_type = "private"
+}
