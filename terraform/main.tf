5067e4d3463de9b5c28b4ab177d711ba99fa1f04
Bilski311 <dominik.bilski24@gmail.com>
2024-03-15 22:26:54 +0100

    Add storage account.

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,6 @@ resource "azurerm_storage_account" "storage_account" {
 
 resource "azurerm_storage_container" "storage_container" {
   name                  = "ml-auto-store-container"
-  storage_account_name  = storage_account.name
+  storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
