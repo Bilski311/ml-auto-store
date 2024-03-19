12cfcd9f1d95ef0528d868373bfd8b79c4242264
Bilski311 <dominik.bilski24@gmail.com>
2024-03-19 20:28:53 +0100

    Add ml workspace.

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,31 +29,31 @@ resource "azurerm_storage_container" "storage_container" {
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
-#
-#resource "azurerm_machine_learning_workspace" "ml_auto_store_workspace" {
-#  name                    = "ml-auto-store-workspace"
-#  location                = azurerm_resource_group.ml_auto_store_rg.location
-#  resource_group_name     = azurerm_resource_group.ml_auto_store_rg.name
-#  storage_account_id      = azurerm_storage_account.storage_account.id
-#  application_insights_id = azurerm_application_insights.app_insights.id
-#  key_vault_id            = azurerm_key_vault.key_vault.id
-#
-#  identity {
-#    type = "SystemAssigned"
-#  }
-#}
-#
-#resource "azurerm_key_vault" "key_vault" {
-#  name                = "autoStoreKv"
-#  location            = azurerm_resource_group.ml_auto_store_rg.location
-#  resource_group_name = azurerm_resource_group.ml_auto_store_rg.name
-#  tenant_id           = var.tenant_id
-#  sku_name            = "standard"
-#}
-#
-#resource "azurerm_application_insights" "app_insights" {
-#  name                = "autoStoreAppInsights"
-#  location            = azurerm_resource_group.ml_auto_store_rg.location
-#  resource_group_name = azurerm_resource_group.ml_auto_store_rg.name
-#  application_type    = "web"
-#}
+
+resource "azurerm_machine_learning_workspace" "ml_auto_store_workspace" {
+  name                    = "ml-auto-store-workspace"
+  location                = azurerm_resource_group.ml_auto_store_rg.location
+  resource_group_name     = azurerm_resource_group.ml_auto_store_rg.name
+  storage_account_id      = azurerm_storage_account.storage_account.id
+  application_insights_id = azurerm_application_insights.app_insights.id
+  key_vault_id            = azurerm_key_vault.key_vault.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_key_vault" "key_vault" {
+  name                = "autoStoreKv"
+  location            = azurerm_resource_group.ml_auto_store_rg.location
+  resource_group_name = azurerm_resource_group.ml_auto_store_rg.name
+  tenant_id           = var.tenant_id
+  sku_name            = "standard"
+}
+
+resource "azurerm_application_insights" "app_insights" {
+  name                = "autoStoreAppInsights"
+  location            = azurerm_resource_group.ml_auto_store_rg.location
+  resource_group_name = azurerm_resource_group.ml_auto_store_rg.name
+  application_type    = "web"
+}
