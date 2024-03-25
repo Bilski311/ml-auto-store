4d36f2370f09f3bb1f3de5178e9cbd6ff1e66401
Bilski311 <dominik.bilski24@gmail.com>
2024-03-25 14:29:36 +0100

    Add compute.

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,14 +68,14 @@ resource "azurerm_application_insights" "app_insights" {
   workspace_id        = azurerm_log_analytics_workspace.log_analytics_workspace.id
   application_type    = "web"
 }
-#
-#resource "azurerm_machine_learning_compute_instance" "ml_auto_store_compute" {
-#  name                          = "ml-auto-store-compute"
-#  location                      = azurerm_resource_group.ml_auto_store_rg.location
-#  machine_learning_workspace_id = azurerm_machine_learning_workspace.ml_auto_store_workspace.id
-#  virtual_machine_size          = "Standard_E4s_v3"
-#
-#  identity {
-#    type = "SystemAssigned"
-#  }
-#}
+
+resource "azurerm_machine_learning_compute_instance" "ml_auto_store_compute" {
+  name                          = "ml-auto-store-compute"
+  location                      = azurerm_resource_group.ml_auto_store_rg.location
+  machine_learning_workspace_id = azurerm_machine_learning_workspace.ml_auto_store_workspace.id
+  virtual_machine_size          = "Standard_E4s_v3"
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
