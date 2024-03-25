91db971c2400c31d39aca0bb65eef0bbd2681c43
Bilski311 <dominik.bilski24@gmail.com>
2024-03-25 12:59:13 +0100

    Delete all.

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,19 +10,19 @@ terraform {
     key                   =  "terraform.tfstate"
   }
 }
-
-resource "azurerm_resource_group" "ml_auto_store_rg" {
-  name     = "ml-auto-store-rg"
-  location = "northeurope"
-}
-
-resource "azurerm_storage_account" "storage_account" {
-  name                     = "autostore321"
-  resource_group_name      = azurerm_resource_group.ml_auto_store_rg.name
-  location                 = azurerm_resource_group.ml_auto_store_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+#
+#resource "azurerm_resource_group" "ml_auto_store_rg" {
+#  name     = "ml-auto-store-rg"
+#  location = "northeurope"
+#}
+#
+#resource "azurerm_storage_account" "storage_account" {
+#  name                     = "autostore321"
+#  resource_group_name      = azurerm_resource_group.ml_auto_store_rg.name
+#  location                 = azurerm_resource_group.ml_auto_store_rg.location
+#  account_tier             = "Standard"
+#  account_replication_type = "LRS"
+#}
 
 #resource "azurerm_storage_container" "storage_container" {
 #  name                  = "ml-auto-store-container"
